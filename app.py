ce4e5bdc1341ae212c83b80a7e37c3fe69f508ec
YiWen Hon <yiwen.hon1@nhs.net>
2024-05-29 15:46:52 +0100

    updated with python 3.11 and rudimentary api key

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_mail import Mail, Message
 import pandas as pd
 from dotenv import load_dotenv
 import os
+import json
 
 load_dotenv()
 app = Flask(__name__)
@@ -33,36 +34,38 @@ def send_emails():
     """
     Basic POST endpoint for API. Accepts JSON of a DataFrame containing the names
     and email addresses of recipients. Columns must be "email_1", "email_2",
-    "name_1" and "name_2".
+    "name_1" and "name_2". Must include valid API key as "key" in query string.
 
     Returns:
         str: Number of emails sent
     """
     data = request.get_json()
-    df = pd.read_json(data, orient="records")
-    print(df)
-    with mail.connect() as conn:
-        count = 0
-        for line in df.index:
-            print(line)
-            count += 1
-            email_1 = df.loc[line, "email_1"]
-            email_2 = df.loc[line, "email_2"]
-            name_1 = df.loc[line, "name_1"]
-            name_2 = df.loc[line, "name_2"]
-            msg = Message(
-                subject="Your Randomized Coffee Trial pairing",
-                recipients=[email_1, email_2],
-            )
-            msg.body = f"""
-                        Dear {name_1} and {name_2}, you are Randomized Coffee 
-                        Trial buddies for this month! Please arrange a 30 minute 
-                        conversation at a mutually convenient time. We hope you 
-                        enjoy it!
-                        """
-            conn.send(msg)
-        return f"{count} messages sent!"
-
+    if data['key'] == os.environ.get("API_KEY"):
+        df = pd.read_json(data['data'], orient="records")
+        with mail.connect() as conn:
+            count = 0
+            for line in df.index:
+                print(line)
+                count += 1
+                email_1 = df.loc[line, "email_1"]
+                email_2 = df.loc[line, "email_2"]
+                name_1 = df.loc[line, "name_1"]
+                name_2 = df.loc[line, "name_2"]
+                msg = Message(
+                    subject="Your Randomized Coffee Trial pairing",
+                    recipients=[email_1, email_2], 
+                    sender=os.environ.get("MAIL_USERNAME"),
+                )
+                msg.body = f"""
+                            Dear {name_1} and {name_2}, you are Randomized Coffee 
+                            Trial buddies for this month! Please arrange a 30 minute 
+                            conversation at a mutually convenient time. We hope you 
+                            enjoy it!
+                            """
+                conn.send(msg)
+            return f"{count} messages sent!"
+    else:
+        return "Invalid API key"
 
 if __name__ == "__main__":
     app.run()
